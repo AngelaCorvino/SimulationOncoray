9a4c71182463645becc9918890ab54cce860d092
Angela Corvino <a.corvino@hzdr.de>
2021-10-11 15:05:46 +0200

    .

--- programs/simminrcf.py
+++ programs/simminrcf.py
@@ -9,10 +9,10 @@ from readcsv import read_datarcf
 from getprofile import  Get_profile
 
 directory='/home/corvin22/Desktop/miniscidom/pictures/2021-09-02/'
-filename=directory+'RCF21CX.csv'
+filename=directory+'RCF21CW.csv'
 depth,rcfdose=read_datarcf(filename)
 rcferr=rcfdose*(546/10000)
-
+area_rcf=np.trapz(rcfdose[0:len(rcfdose)], depth[0:len(depth)])
 #filename1=directory+'RCF21CQ.csv'
 #depth1,rcfdose1=read_datarcf(filename1)
 #rcferr1=rcfdose1*(546/10000)
@@ -27,15 +27,16 @@ rcferr=rcfdose*(546/10000)
 
 directory='/home/corvin22/Desktop/miniscidom/pictures/2021-09-02/notnormalized/'
 
-data= np.load(directory+'notnormalizedmean_array17.npy')
+data= np.load(directory+'notnormalizedmean_array1.npy')
 dose=data[0:len(data)-3]
 
 #tof1=data1[len(data1)-3:len(data1)]
 unidose=0
-data0= np.load(directory+'notnormalizederr17.npy')
+data0= np.load(directory+'notnormalizederr1.npy')
 err=data0[0:len(data0)]
 #top_projection_dose= np.load(directory+'1Dtopprojection/'+'top1Dmean_array1.npy')
-
+x= np.arange(0,len(dose),1)*0.0738255
+area=np.trapz(dose[3:len(dose)],x[3:len(x)]) #eliminate last 3 points
 
 """
 directory='/home/corvin22/Desktop/miniscidom/pictures/2021-09-01/notnormalized/'
@@ -58,47 +59,58 @@ err2=data2[0:len(data2)]
 
 
 # read in the csv-file
-outputfile_topas = '/home/corvin22/SimulationOncoray/data/Single/DoseToWater_90MeVproton_PVT_6PC_1Dscorer.csv'
+#outputfile_topas = '/home/corvin22/SimulationOncoray/data/SOBP/DoseToWater_150MeVproton_PVT_12PC_SOBP_1Dscorer.csv'
+outputfile_topas = '/home/corvin22/SimulationOncoray/data/Single/DoseToWater_90MeVproton_PVT_6PC1PMMA_1Dscorer.csv'
 header = pd.read_csv(outputfile_topas, nrows = 7)
 df = pd.read_csv(outputfile_topas, comment='#', header=None)
 topas_datamatrix = np.array(df)# convert dataframe df to array
 
+numberof_xbins = 1
+numberof_zbins = 149
+doseprofile=Get_profile(topas_datamatrix, 149,1)
+xdoseprofile=doseprofile.xmeanprofile
+zdoseprofile=doseprofile.zmeanprofile
+zdoseprofile=zdoseprofile[::-1]
+depth_sci= np.arange(0,len(zdoseprofile),1)*0.0738255
+area_sci=np.trapz(zdoseprofile[3:len(zdoseprofile)],depth_sci[3:len(depth_sci)]) #eliminate last 3 points
+
+
+"""
 # read in the csv-file
-outputfile_topas = '/home/corvin22/SimulationOncoray/data/Single/EnergyDeposit_90MeVproton_PVT_6PC_1Dscorer.csv'
+outputfile_topas = '/home/corvin22/SimulationOncoray/data/SOBP/EnergyDeposit_150MeVproton_PVT_12PC_SOBP_1Dscorer.csv'
 header = pd.read_csv(outputfile_topas, nrows = 7)
 df = pd.read_csv(outputfile_topas, comment='#', header=None)
 topas_datamatrix_energy = np.array(df)# convert dataframe df to array
-print(np.shape(topas_datamatrix))
-
 numberof_xbins = 1
 numberof_zbins = 149
-profile=Get_profile(topas_datamatrix, 149,1)
-xmeanprofile=profile.xmeanprofile
-zmeanprofile=profile.zmeanprofile
-zmeanprofile=zmeanprofile[::-1]
+energyprofile=Get_profile(topas_datamatrix_energy, 149,1)
+xenergyprofile=energyprofile.xmeanprofile
+zenergyprofile=energyprofile.zmeanprofile
+zenergyprofile=zenergyprofile[::-1]
 
-
-
+#area_sci=np.trapz(zenergprofile[3:len(zdoseprofile)],depth_sci[3:len(depth_sci)]) #eliminate last 3 points
+"""
 
 plt.figure(1) # creates a figure in which we plot
 #plt.plot(np.arange(0,numberof_xbins,1)*0.0634, xmeanprofile/np.max(xmeanprofile),'.-',label='{mean value of 170 bins along z}') # plots depth on x, dose on y, and uses default layout
 plt.plot(np.arange(0,numberof_zbins,1)*0.0738255,
-                                            zmeanprofile/np.max(zmeanprofile),
+                                                          zdoseprofile/area_sci,
                                                                             '.',
-                                                                            markersize=8,
-                                        label='{ TOPAS Simulation}')
+                                                                    markersize=8,
+                                                        label='Simulation in miniscidom ')
 
 
 
-plt.errorbar(  np.arange(0,len(dose),1)*0.074,                         dose/dose.max() ,
-                                                                      yerr=err/dose.max(),
+
+plt.errorbar(np.arange(0,len(dose),1)*0.0738255,                         dose/area ,
+                                                                      yerr=err/area,
                                                                       xerr=None,
                                                                         fmt='.',
                                                                         color='green',
                                                                         markersize=8,
                                                                    ecolor='green',
                                                                 elinewidth=None,
-                                            label=' reconstruction '.format(unidose))
+                                            label=' Reconstruction '.format(unidose))
 """
 
 plt.errorbar(  np.arange(0,len(dose1),1)*0.074,                         dose1/dose1.max() ,
@@ -128,19 +140,19 @@ plt.fill_between(depth,
                                                                 rcfdose1+rcferr1,
                                                                 color='orange', alpha=0.1)
 """
-plt.errorbar( depth,                      rcfdose/rcfdose.max() ,
-                                                                  yerr=rcferr/rcfdose.max(),
+plt.errorbar(depth,                      rcfdose/area_rcf,
+                                                                  yerr=rcferr/area_rcf,
                                                                       xerr=None,
                                                                         fmt='.',
                                                                         color='red',
                                                                         markersize=8,
                                                                    ecolor='red',
                                                                 elinewidth=None,
-                                                                label=' RCF 21CX measured dose')
+                                                                label=' RCF 21CW')
 
 plt.fill_between(depth,
-                                                                rcfdose/rcfdose.max()-rcferr/rcfdose.max(),
-                                                                rcfdose/rcfdose.max()+rcferr/rcfdose.max(),
+                                                                rcfdose/area_rcf-rcferr/area_rcf,
+                                                                rcfdose/area_rcf+rcferr/area_rcf,
                                                         color='red', alpha=0.1)
 
 
@@ -155,7 +167,7 @@ plt.fill_between(depth,
 
 
 #plt.plot(depthPVT, dosePVT,'.-',label='{70 MeV protons ,0cm distance}') # plots depth on x, dose on y, and uses default layout
-plt.title('Depht dose distribution(aperture diameter= 7mm, 6PC)') # Title
+plt.title('Depht dose distribution(aperture diameter= 7mm, 6PC+1PMMA)') # Title
 plt.xlabel('Depth in Water x direction [mm]') # label for x-axis
 plt.ylabel('Relative Dose in water ') # label for y axis
 
